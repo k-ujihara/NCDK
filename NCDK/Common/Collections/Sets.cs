a50c6c41f742d1f4bc35aa6724e84e3250909474
Kazuya Ujihara <ujihara.kazuya@gmail.com>
2017-08-06 11:21:03 +0900

    Seal empty collections

--- NCDK/Common/Collections/Sets.cs
+++ NCDK/Common/Collections/Sets.cs
@@ -1,55 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace NCDK.Common.Collections
 {
-    public class EmptyEnumerator<T> : IEnumerator<T>
-    {
-        public T Current { get { throw new InvalidOperationException(); } }
-        object IEnumerator.Current { get { throw new InvalidOperationException(); } }
-        public void Dispose() { }
-        public bool MoveNext() => false;
-        public void Reset() { }
-    }
-
-    public class EmptyEnumerable<T> : IEnumerable<T>
-    {
-        static EmptyEnumerator<T> enumerator = new EmptyEnumerator<T>();
-
-        public IEnumerator<T> GetEnumerator() => enumerator;
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-    }
-
-    public class EmptyCollection<T> : EmptyEnumerable<T>, ICollection<T>
-    {
-        public int Count => 0;
-        public bool IsReadOnly => true;
-        public void Add(T item) { throw new InvalidOperationException(); }
-        public void Clear() { }
-        public bool Contains(T item) => false;
-        public void CopyTo(T[] array, int arrayIndex) { }
-        public bool Remove(T item) => false;
-    }
-
-    public class EmptySet<T> : EmptyCollection<T>, ISet<T>
-    {
-        internal static EmptySet<T> instance = new EmptySet<T>();
-
-        public void ExceptWith(IEnumerable<T> other) { }
-        public void IntersectWith(IEnumerable<T> other) { }
-        public bool IsProperSubsetOf(IEnumerable<T> other) => true;
-        public bool IsProperSupersetOf(IEnumerable<T> other) => false;
-        public bool IsSubsetOf(IEnumerable<T> other) => true;
-        public bool IsSupersetOf(IEnumerable<T> other) => false;
-        public bool Overlaps(IEnumerable<T> other) => false;
-        public bool SetEquals(IEnumerable<T> other) => other.Count() == 0;
-        public void SymmetricExceptWith(IEnumerable<T> other) { }
-        public void UnionWith(IEnumerable<T> other) { throw new InvalidOperationException(); }
-        bool ISet<T>.Add(T item) { throw new InvalidOperationException(); }
-    }
-
     public class Sets
     {
         public static ISet<T> Empty<T>() => EmptySet<T>.instance;
