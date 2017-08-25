a67ea4b6b6739637c01c46e4cb44a9c66a5cb570
Kazuya Ujihara <ujihara.kazuya@gmail.com>
2017-08-26 00:32:58 +0900

    Add ACDK project

---
+++ ACDKTests/ChemObjectBuilderTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACDK
+{
+    [TestClass()]
+    public class ChemObjectBuilderTests
+    {
+        [TestMethod()]
+        public void ChemObjectBuilderTest()
+        {
+            var a = new W_IChemObjectBuilder(NCDK.Default.ChemObjectBuilder.Instance);
+            var b = (IChemObjectBuilder)a;
+        }
+    }
+}
---
+++ ACDKTests/ExcelTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NCDK;
+using System;
+using System.IO;
+
+namespace NCDK.Tests
+{
+    [TestClass()]
+    public class ExcelTests
+    {
+        [TestCategory("Excel")]
+        [TestMethod()]
+        public void ExcelTest()
+        {
+            string filename = "ACDK_Test.xlsm";
+            bool passed = false;
+#if HAS_EXCEL_REFERENCE
+            MSExcel.Application excel = new MSExcel.Application();
+#else
+            dynamic excel = Activator.CreateInstance(Type.GetTypeFromProgID("Excel.Application"));
+#endif
+            try
+            {
+                excel.Visible = true;
+                excel.Workbooks.Open(Path.Combine(Path.GetTempPath(), filename));
+                excel.Application.Run(filename + "!TestAll");
+                passed = true;
+            }
+            finally
+            {
+                try
+                {
+                    excel.Quit();
+                }
+                catch (Exception)
+                { }
+            }
+            Assert.IsTrue(passed, "Excel test failed.");
+        }
+    }
+}
