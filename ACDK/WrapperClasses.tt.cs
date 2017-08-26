45db5a52c7c439e81016ce4b3c030a8ac4ccb11a
Kazuya Ujihara <ujihara.kazuya@gmail.com>
2017-08-26 23:49:56 +0900

    Separate COM info to Class_Info.txt file

--- ACDK/WrapperClasses.tt.cs
+++ ACDK/WrapperClasses.tt.cs
@@ -20,7 +20,6 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-
 using System;
 using System.Runtime.InteropServices;
 
@@ -183,4 +182,3 @@ namespace ACDK
         }
     }
 }
-
