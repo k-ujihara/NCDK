7fe9bad52ff83d17190edbead624df4aebff2262
Kazuya Ujihara <ujihara.kazuya@gmail.com>
2017-08-27 15:22:49 +0900

    Add signing

--- NCDKDisplay/Properties/AssemblyInfo.cs
+++ NCDKDisplay/Properties/AssemblyInfo.cs
@@ -15,4 +15,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("NCDKDisplayTests")]
+[assembly: InternalsVisibleTo("NCDKDisplayTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100e5414bbfe7bfd9cf179af307bc50ff50b5b620795dae0b2830e851337ea03603c9ee0ea32c1ad04ba1657ba8afb8e078e2fde7b732c7315d789dadf371969713d5973a7a8973a4b4781685f03088d0891bad8057a7d1d8b7ff3c211b5160701c5344342b1dc8327b08304f6036427b534e746d3553539fc6d4a01d73afccf7e7")]
