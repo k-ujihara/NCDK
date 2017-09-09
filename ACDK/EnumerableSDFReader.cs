b3fa64d957487652f43a633ae9a121964e42d2d0
Kazuya Ujihara <ujihara.kazuya@gmail.com>
2017-09-09 09:10:54 +0900

    Add EnumerableSDFReader and FileDialog class

---
+++ ACDK/EnumerableSDFReader.cs
@@ -0,0 +1,55 @@
+﻿using NCDK;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ACDK
+{
+    [Guid("DAF6D958-C01B-4289-9673-3BE3DBB4E2B9")]
+    [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    public interface IEnumerableSDFReader
+    {
+        [DispId(0x2001)]
+        void Open(string filename, IChemObjectBuilder builder);
+        [DispId(0x2002)]
+        void Close();
+        [DispId(0x2003)]
+        IAtomContainer Read();
+    }
+
+    [Guid("2AB919D2-C897-4111-B175-BD5517185BBE")]
+    [ComDefaultInterface(typeof(IEnumerableSDFReader))]
+    public sealed class EnumerableSDFReader
+        : IEnumerableSDFReader
+    {
+        NCDK.IO.Iterator.EnumerableSDFReader obj;
+        public NCDK.IO.Iterator.EnumerableSDFReader Object => obj;
+        IEnumerator<NCDK.IAtomContainer> enumerator;
+
+        public EnumerableSDFReader()
+        { }
+
+        [DispId(0x2001)]
+        public void Open(string filename, IChemObjectBuilder builder)
+        {
+            obj = new NCDK.IO.Iterator.EnumerableSDFReader(new StreamReader(filename), ((W_IChemObjectBuilder)builder).Object);
+            enumerator = obj.GetEnumerator();
+        }
+
+        [DispId(0x2002)]
+        public void Close()
+        {
+            enumerator.Dispose();
+            obj.Close();
+        }
+
+        [DispId(0x2003)]
+        public IAtomContainer Read()
+        {
+            if (enumerator.MoveNext())
+                return new W_IAtomContainer(enumerator.Current);
+            return null;
+        }
+    }
+}
---
+++ ACDK/FileDialog.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ACDK
+{
+    [Guid("3D9B183C-89E6-4554-90C5-7242450AB23E")]
+    [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    public interface IFileDialog
+    {
+        [DispId(0x2001)]
+        string OpenFile(string initialDirectory);
+    }
+
+    [Guid("E5170650-539E-41DF-A9E4-CA55D420EF0A")]
+    [ComDefaultInterface(typeof(IFileDialog))]
+    public sealed class FileDialog
+        : IFileDialog
+    {
+        [DispId(0x2001)]
+        public string OpenFile(string initialDirectory)
+        {
+            var dialog = new OpenFileDialog()
+            {
+                InitialDirectory = initialDirectory
+            };
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                return dialog.FileName;
+            }
+            return null;
+        }
+    }
+}
