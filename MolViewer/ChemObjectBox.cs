96c34f99ef785b7aa53b2b8f6db6bce258bcad8e
Kazuya Ujihara <ujihara.kazuya@gmail.com>
2018-01-17 22:19:34 +0900

    Add ChemObjectBox

---
+++ MolViewer/ChemObjectBox.cs
@@ -0,0 +1,50 @@
+﻿using NCDK.Depict;
+using System.Windows.Media;
+
+namespace NCDK.Controls
+{
+    public class ChemObjectBox : System.Windows.Controls.Image
+    {
+        public DepictionGenerator Generator { get; } = new DepictionGenerator();
+        private IChemObject _ChemObject = null;
+
+
+        public IChemObject ChemObject
+        {
+            get => _ChemObject;
+            set
+            {
+                if (_ChemObject != value)
+                {
+                    _ChemObject = value;
+
+                    Depiction depiction;
+                    switch (ChemObject)
+                    {
+                        case IAtomContainer mol:
+                            depiction = Generator.Depict(mol);
+                            break;
+                        case IReaction rxn:
+                            depiction = Generator.Depict(rxn);
+                            break;
+                        default:
+                            depiction = null;
+                            break;
+                    }
+
+                    if (depiction != null)
+                    {
+                        var drawingVisual = new DrawingVisual();
+                        using (var g2 = drawingVisual.RenderOpen())
+                        {
+                            depiction.Draw(g2);
+                        }
+                        Source = new DrawingImage(drawingVisual.Drawing);
+                    }
+
+                    this.InvalidateVisual();
+                }
+            }
+        }
+    }
+}
