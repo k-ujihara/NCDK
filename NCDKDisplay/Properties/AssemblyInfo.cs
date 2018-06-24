ebad916e53765516dc98bd7b38f4cc425e8aa746
Kazuya Ujihara <ujihara.kazuya@gmail.com>
2018-06-24 12:31:03 +0900

    Update documents

--- NCDKDisplay/Properties/AssemblyInfo.cs
+++ NCDKDisplay/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Kazuya Ujihara")]
 [assembly: AssemblyCopyright("Copyright © 2016-2018 Kazuya Ujihara")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
